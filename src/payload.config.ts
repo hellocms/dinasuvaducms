0d7961f70f437c4f4afc1e9c7518569153e3e0fa
Hello-CMS-AI <hellocms.ai@gmail.com>
2025-06-09 16:16:02 +0530

    Initial commit

--- src/payload.config.ts
+++ src/payload.config.ts
@@ -107,6 +107,7 @@ export default buildConfig({
         region: process.env.S3_REGION || '',
         endpoint: process.env.S3_ENDPOINT || '',
       },
+      acl: 'public-read',
     }),
   ],
   secret: process.env.PAYLOAD_SECRET,
